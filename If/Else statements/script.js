2aaf5d599635a28edf275af0bb4e9ad51b2b790e
HibaBenAmmar <86246905+HibaBenAmmar@users.noreply.github.com>
2021-07-06 14:04:12 +0200

    Add files via upload

---
+++ If/Else statements/script.js
@@ -0,0 +1,31 @@
+//Kroegbaas: personen onder de 18 jaar mogen niet naar binnen.
+
+const age = 17;
+
+if (age >= 18) {
+    console.log("Deze persoon mag naar binnen");
+    
+}else { 
+    console.log ("Deze persoon is te jong en wordt vriendelijk verzocht om buiten te blijven");
+
+}
+//Ladiesnight: alleen vrouwen mogen naar binnen.
+
+const isFemale = "vrouw";
+
+if (isFemale = ) {
+    console.log ("U heeft toegang als vrouw");
+    
+}else {
+    console.log ("U heeft helaas geen toegang");
+}
+//Kroegbaas: veilig thuis komen met de 'bob'.
+
+const driverStatus = "bob";
+
+if (driverStatus === "bob") {
+    console.log ("U bent de bob");
+    
+}else {
+    console.log ("U bent niet de bob en mag helaas niet rijden");
+}
